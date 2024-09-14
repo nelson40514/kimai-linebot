bed5695d64c349e99a56cd3b6bdf942cbcfa23e7
NelsonSu <nelson40514@gmail.com>
2024-09-14 03:05:14 +0000

    Vercel modify

--- api/index.py
+++ api/index.py
@@ -1,108 +1,49 @@
-import os
-import sys
-
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-from linebot.v3 import (
-    WebhookHandler
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent,
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ConfirmTemplate,
-    TextMessage,
-    TemplateMessage,
-    ReplyMessageRequest,
-    ApiException,
-    QuickReply,
-    QuickReplyItem,
-    MessageAction,
-)
+import os
+import time
+from dotenv import load_dotenv
+
+# 載入.env檔案中的變數
+load_dotenv()
+
+
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 app = Flask(__name__)
 
-# 設置環境變數
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None or channel_access_token is None:
-    print('Specify LINE_CHANNEL_SECRET and LINE_CHANNEL_ACCESS_TOKEN as environment variables.')
-    sys.exit(1)
+# domain root
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-
-# 初始化 LineBot
-handler = WebhookHandler(channel_secret)
-
-configuration = Configuration(
-    access_token=channel_access_token
-)
-
-
-
-@app.route("/")
-def index():
-    # Return request info
-    res = "From Kimai Assistant<br>"
-    res += "Request Info:<br>"
-    res += "Host: " + request.host + "<br>"
-    res += "URL: " + request.url + "<br>"
-    res += "Base URL: " + request.base_url + "<br>"
-    res += "Remote Addr: " + request.remote_addr + "<br>"
-    res += "Method: " + request.method + "<br>"
-    res += "Path: " + request.path + "<br>"
-    res += "Full Path: " + request.full_path + "<br>"
-    res += "Query String: " + request.query_string.decode("utf-8") + "<br>"
-    res += "Headers: <br>"
-    for key, value in request.headers:
-        res += key + ": " + value + "<br>"
-    res += "Data: " + request.data.decode("utf-8") + "<br>"
-    
-    return res
-
-@handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event):
-    text = event.message.text
-    user_id = event.source.user_id
-
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        
-        # 預設快速回覆選單
-        line_bot_api.reply_message(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[
-                    TextMessage(text=f"Hello {user_id}, {text}"),
-                ]
-            )
-        )
-
-
-@app.route("/callback", methods=['POST'])
+@app.route("/webhook", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
-        handler.handle(body, signature)
-    except ApiException as e:
-        app.logger.warning("Got exception from LINE Messaging API: %s\n" % e.body)
+        line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    time.sleep(1)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
